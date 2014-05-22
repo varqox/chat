6e021469ca13d351a88c0b836610ec2d8c4fd130
cytadela8 <cytadela8@op.pl>
2014-05-22 19:32:03 +0200

    Word braking fixed

--- src/index.php
+++ src/index.php
@@ -80,6 +80,11 @@
 		font-size: 15px;
 		padding: 2px;
 		word-break: break-word;
+		white-space: pre-wrap; /* css-3 */
+		white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
+		white-space: -pre-wrap; /* Opera 4-6 */
+		white-space: -o-pre-wrap; /* Opera 7 */
+		word-wrap: break-word;
 	}
 	.code
 	{
